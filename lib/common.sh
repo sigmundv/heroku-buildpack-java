122eb5b38e72b5b2b6e8ef776a33db69e6353ffa
Sigmund Vestergaard <sigmund.vestergaard@ie.ibm.com>
2018-05-29 00:33:02 +0100

    Increased curl timeout in maven download to 120 seconds

--- lib/common.sh
+++ lib/common.sh
@@ -30,7 +30,7 @@ download_maven() {
   local installDir=$2
   local mavenHome=$3
   rm -rf $mavenHome
-  curl --retry 3 --silent --max-time 60 --location ${mavenUrl} | tar xzm -C $installDir
+  curl --retry 3 --silent --max-time 120 --location ${mavenUrl} | tar xzm -C $installDir
   chmod +x $mavenHome/bin/mvn
 }
 
